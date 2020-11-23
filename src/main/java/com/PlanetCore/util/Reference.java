67857305daa0625b1c3a0df215e7f032a977f6d2
suprememarshal <vetaking@hotmail.com>
2020-11-22 20:34:34 -0500

    pre-alpha 005

--- src/main/java/com/PlanetCore/util/Reference.java
+++ src/main/java/com/PlanetCore/util/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "planetcore";
 	public static final String NAME = "Planet Core Pre-Alpha";
-	public static final String VERSION = "0.0.4";
+	public static final String VERSION = "0.0.5";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
 	public static final String CLIENT = "com.PlanetCore.proxy.ClientProxy";
 	public static final String SERVER = "com.PlanetCore.proxy.CommonProxy";
