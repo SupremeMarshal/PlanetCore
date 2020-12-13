52d2de5e8f1e0af67673b33f77233bcf26862966
suprememarshal <vetaking@hotmail.com>
2020-12-13 15:32:38 -0500

    Alpha 009

--- src/main/java/com/PlanetCore/util/Reference.java
+++ src/main/java/com/PlanetCore/util/Reference.java
@@ -4,8 +4,8 @@ public class Reference {
 
 	
 	public static final String MOD_ID = "planetcore";
-	public static final String NAME = "Planet Core Pre-Alpha";
-	public static final String VERSION = "0.0.8";
+	public static final String NAME = "Planet Core Alpha";
+	public static final String VERSION = "0.0.9";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
 	public static final String CLIENT = "com.PlanetCore.proxy.ClientProxy";
 	public static final String SERVER = "com.PlanetCore.proxy.CommonProxy";
