d8dd9404f4dbe1b6595e2a95fb31e14d88f10f90
suprememarshal <vetaking@hotmail.com>
2020-12-14 13:18:38 -0500

    some json fixes

--- src/main/java/com/PlanetCore/util/Reference.java
+++ src/main/java/com/PlanetCore/util/Reference.java
@@ -4,8 +4,8 @@ public class Reference {
 
 	
 	public static final String MOD_ID = "planetcore";
-	public static final String NAME = "Planet Core Alpha";
-	public static final String VERSION = "010";
+	public static final String NAME = "Planetcore Alpha";
+	public static final String VERSION = "11";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
 	public static final String CLIENT = "com.PlanetCore.proxy.ClientProxy";
 	public static final String SERVER = "com.PlanetCore.proxy.CommonProxy";
