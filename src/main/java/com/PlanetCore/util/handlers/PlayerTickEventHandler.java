b5d19c5132d7d3946a6028d5104e071fb87f2534
suprememarshal <vetaking@hotmail.com>
2020-12-21 12:12:13 -0500

    Potential fix to lava burning from distance

--- src/main/java/com/PlanetCore/util/handlers/PlayerTickEventHandler.java
+++ src/main/java/com/PlanetCore/util/handlers/PlayerTickEventHandler.java
@@ -42,60 +42,60 @@ public class PlayerTickEventHandler {
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 
-        //Heal player based on food level. Heal from 0% to 100% in 600 seconds at max food lvl. Heal fully in 3 hours if food level is 1. At 0 food, player stop healing.
-        if (event.player.getFoodStats().getFoodLevel() >= 1) {
-        if (event.player.world.getTotalWorldTime() % 20 == 1) {
-            event.player.heal(event.player.getMaxHealth() / (12000 / event.player.getFoodStats().getFoodLevel()));
-        }
+        if (!event.player.getEntityWorld().isRemote) {
+            //Heal player based on food level. Heal from 0% to 100% in 600 seconds at max food lvl. Heal fully in 3 hours if food level is 1. At 0 food, player stop healing.
+            if (event.player.getFoodStats().getFoodLevel() >= 1) {
+                if (event.player.world.getTotalWorldTime() % 20 == 1) {
+                    event.player.heal(event.player.getMaxHealth() / (12000 / event.player.getFoodStats().getFoodLevel()));
+                }
 
-        }
+            }
 
 
-        int x,y,z;
-        x = event.player.getPosition().getX();
-        y = event.player.getPosition().getY();
-        z = event.player.getPosition().getZ();
-        Iterable<BlockPos> it = BlockPos.getAllInBox(x - 4, y - 4, z - 4, x + 4, y + 4, z + 4);
-        int randomLava = new Random().nextInt(30) + 1;
-        if (randomLava == 1) {
-            for (BlockPos pos : it) {
-                IBlockState state = event.player.world.getBlockState(pos);
-                if (state.getMaterial() == Material.LAVA) {
+            int x, y, z;
+            x = event.player.getPosition().getX();
+            y = event.player.getPosition().getY();
+            z = event.player.getPosition().getZ();
+            Iterable<BlockPos> it = BlockPos.getAllInBox(x - 4, y - 4, z - 4, x + 4, y + 4, z + 4);
+            int randomLava = new Random().nextInt(30) + 1;
+            if (randomLava == 1) {
+                for (BlockPos pos : it) {
+                    IBlockState state = event.player.world.getBlockState(pos);
+                    if (state.getMaterial() == Material.LAVA) {
 
-                    if (event.player.world.getTotalWorldTime() % 5 == 1) {
-                        event.player.setFire(5);
-                        event.player.attackEntityFrom(DamageSource.LAVA,1.0F);
+                        if (event.player.world.getTotalWorldTime() % 5 == 1) {
+                            event.player.setFire(5);
+                            event.player.attackEntityFrom(DamageSource.LAVA, 1.0F);
+                        }
+                    }
+                }
+                for (BlockPos pos1 : it) {
+                    IBlockState state1 = event.player.world.getBlockState(pos1);
+                    if (state1.getBlock() == ModBlocks.HOT_LAVA_FLUID) {
+                        if (event.player.world.getTotalWorldTime() % 5 == 1) {
+                            event.player.setFire(15);
+                            event.player.attackEntityFrom(DamageSource.LAVA, 5.0F);
+                        }
                     }
                 }
             }
-            for (BlockPos pos1 : it) {
-                IBlockState state1 = event.player.world.getBlockState(pos1);
-                if (state1.getBlock() == ModBlocks.HOT_LAVA_FLUID) {
-                    if (event.player.world.getTotalWorldTime() % 5 == 1) {
-                        event.player.setFire(15);
-                        event.player.attackEntityFrom(DamageSource.LAVA,5.0F);
-                    }
+
+
+            int random = new Random().nextInt(5) + 1;
+            if (event.player.posY < -600) {
+                if (event.player.world.getTotalWorldTime() % (int) (-600000 * random / (event.player.posY + 500)) != 1) {
+                    return;
                 }
-            }
-        }
+                if (!event.player.isImmuneToFire()) {
+                    event.player.setFire((int) (event.player.posY / -250));
+                }
+                event.player.attackEntityFrom(DamageSource.LAVA, (float) (-1 * event.player.posY / 500));
+                //every A seconds, 1/10 chance to burn player for B seconds below -1k Y.
+                //A = (1 minute/(latitude+500)*-500);  (-1k Y = 1-5 minute,  -2k Y = 20-100 seconds, -5k = 6.67-33 seconds, -10k = 3.16-15.8 seconds.)
+                //B = (latitude/-100-9); (At -1k latitude, burn for 2 second. at -1500, burn for 6 seconds. At -5k, burn for 41 seconds.
+                //Damage = 5 damage every 1k distance downward. Starting at -1k. -2k = 10 damages, -5k = 25 damages, -10k = 50 damages.
 
-
-        int random = new Random().nextInt(5)+1;
-        if (event.player.posY < -600)
-        {
-            if (event.player.world.getTotalWorldTime() % (int)(-600000*random/(event.player.posY+500)) != 1)
-            {
-                return;
             }
-            if (!event.player.isImmuneToFire()) {
-                event.player.setFire((int)(event.player.posY/-250));
-            }
-            event.player.attackEntityFrom(DamageSource.LAVA, (float) (-1*event.player.posY/500));
-            //every A seconds, 1/10 chance to burn player for B seconds below -1k Y.
-            //A = (1 minute/(latitude+500)*-500);  (-1k Y = 1-5 minute,  -2k Y = 20-100 seconds, -5k = 6.67-33 seconds, -10k = 3.16-15.8 seconds.)
-            //B = (latitude/-100-9); (At -1k latitude, burn for 2 second. at -1500, burn for 6 seconds. At -5k, burn for 41 seconds.
-            //Damage = 5 damage every 1k distance downward. Starting at -1k. -2k = 10 damages, -5k = 25 damages, -10k = 50 damages.
-
         }
     }
 
