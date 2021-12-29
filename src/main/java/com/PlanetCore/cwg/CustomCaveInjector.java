dad929d31ca2acc0787190138aa1bb315f71d307
SupremeMarshal <vetaking@hotmail.com>
2021-12-29 03:59:38 -0500

    added a missing noise cave back

--- src/main/java/com/PlanetCore/cwg/CustomCaveInjector.java
+++ src/main/java/com/PlanetCore/cwg/CustomCaveInjector.java
@@ -10,7 +10,8 @@ public class CustomCaveInjector {
             case CAVE:
                 event.setNewGen(new WrappedStructureGenerator(
                         new ImprovedCaveGenerator(),
-                        new NoiseCaveGenerator2()
+                        new NoiseCaveGenerator2(),
+                        new NoiseCaveGenerator3()
                 ));
                 break;
             case RAVINE:
