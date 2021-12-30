319d5f559add1b384e4817a0342bf11061784e8d
Tom Martin <tom.martin1239@gmail.com>
2021-12-30 04:32:10 +0000

    Prevent entities spawning below y=0

---
+++ src/main/java/com/PlanetCore/asm/mixin/MixinEntityAnimal.java
@@ -0,0 +1,26 @@
+package com.PlanetCore.asm.mixin;
+
+import net.minecraft.entity.passive.EntityAnimal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(EntityAnimal.class)
+public class MixinEntityAnimal {
+    @Inject(method = "getCanSpawnHere",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/passive/EntityAnimal;world:Lnet/minecraft/world/World;",
+                    ordinal = 0
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void minimumSpawnHeight(CallbackInfoReturnable<Boolean> cir, int x, int y, int z) {
+        if(y < 0) {
+            cir.setReturnValue(false);
+        }
+        //otherwise, default to the vanilla behaviour
+    }
+}
