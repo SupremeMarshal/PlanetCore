3a48ecb3117e28d061654d736d58df415aaad5e6
Tom Martin <tom.martin1239@gmail.com>
2021-12-30 04:31:36 +0000

    Add mixin dependency, fix http forge maven

---
+++ src/main/java/com/PlanetCore/asm/PlanetCoreFMLLoadingPlugin.java
@@ -0,0 +1,44 @@
+package com.PlanetCore.asm;
+
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class PlanetCoreFMLLoadingPlugin implements IFMLLoadingPlugin {
+    public PlanetCoreFMLLoadingPlugin() {
+        FMLLog.log.info("PlanetCore Mixin init");
+        MixinBootstrap.init();
+
+        Mixins.addConfiguration("mixins.planetcore.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
