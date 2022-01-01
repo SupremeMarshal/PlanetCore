e63678910e3e2932e32cc632e912327a207f283f
SupremeMarshal <vetaking@hotmail.com>
2021-12-31 22:39:11 -0500

    Passive entities don't spawn below Y=0

---
+++ src/main/java/com/PlanetCore/util/handlers/EntitySpawnEvent.java
@@ -0,0 +1,23 @@
+package com.PlanetCore.util.handlers;
+
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod.EventBusSubscriber(modid="planetcore")
+public class EntitySpawnEvent {
+
+    @SubscribeEvent
+    public void onCheckSpawnClient(LivingSpawnEvent.CheckSpawn cs)
+    {
+        if (cs.getEntity().posY < 0 && (!cs.getEntity().isCreatureType(EnumCreatureType.MONSTER, true) || !cs.getEntity().isCreatureType(EnumCreatureType.MONSTER, false)))
+            cs.setResult(Event.Result.DENY);
+        else
+            cs.setResult(Event.Result.DEFAULT);
+    }
+
+}
