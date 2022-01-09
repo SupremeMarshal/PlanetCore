a81d8e017e1f1c18dad3ea05f08e285bae33563f
SupremeMarshal <vetaking@hotmail.com>
2022-01-09 15:49:56 -0500

    Core and onyx demon spawn less often

--- src/main/java/com/PlanetCore/init/EntityInit.java
+++ src/main/java/com/PlanetCore/init/EntityInit.java
@@ -55,8 +55,8 @@ public class EntityInit
 		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + name), entity, name, id, Main.instance, range, 1, true, color1, color2);
 		Biome[] spawnBiomes = allbiomes(Biome.REGISTRY);
 		EntityRegistry.addSpawn(EntityDemon.class, 6, 1, 3, EnumCreatureType.MONSTER, spawnBiomes);
-		EntityRegistry.addSpawn(EntityCoreDemon.class, 4, 1, 1, EnumCreatureType.MONSTER, spawnBiomes);
-		EntityRegistry.addSpawn(EntityOnyxDemon.class, 2, 1, 1, EnumCreatureType.MONSTER, spawnBiomes);
+		EntityRegistry.addSpawn(EntityCoreDemon.class, 2, 1, 1, EnumCreatureType.MONSTER, spawnBiomes);
+		EntityRegistry.addSpawn(EntityOnyxDemon.class, 1, 1, 1, EnumCreatureType.MONSTER, spawnBiomes);
 		EntityRegistry.addSpawn(EntityGhoul.class, 10, 1, 2, EnumCreatureType.MONSTER, spawnBiomes);
 		EntityRegistry.addSpawn(EntityHellHound.class, 9, 1, 3, EnumCreatureType.MONSTER, spawnBiomes);
 		EntityRegistry.addSpawn(EntityDeathEnderman.class, 5, 1, 1, EnumCreatureType.MONSTER, spawnBiomes);
