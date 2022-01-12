e493542c24152f981cfeb90d0f73e75b150f0ffb
SupremeMarshal <vetaking@hotmail.com>
2022-01-12 12:47:25 -0500

    Version 1.0.7

--- src/main/java/com/PlanetCore/util/Reference.java
+++ src/main/java/com/PlanetCore/util/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "planetcore";
 	public static final String NAME = "Planetcore";
-	public static final String VERSION = "1.0.6";
+	public static final String VERSION = "1.0.7";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
 	public static final String CLIENT = "com.PlanetCore.proxy.ClientProxy";
 	public static final String SERVER = "com.PlanetCore.proxy.CommonProxy";
