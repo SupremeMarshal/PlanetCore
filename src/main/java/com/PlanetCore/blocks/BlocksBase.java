7aa0d7d2887025d62c0b29ba68f552f8e20be5c2
SupremeMarshal <vetaking@hotmail.com>
2022-01-22 14:50:54 -0500

    fix Supercompact ore hardness

--- src/main/java/com/PlanetCore/blocks/BlocksBase.java
+++ src/main/java/com/PlanetCore/blocks/BlocksBase.java
@@ -34,7 +34,7 @@ public class BlocksBase extends BlockBase {
 
 	@Override
 	public float getBlockHardness(IBlockState blockState, World worldIn, BlockPos pos) {
-		return planetHardness.hardness * 64;
+		return planetHardness.hardness;
 	}
 
 	@Override
