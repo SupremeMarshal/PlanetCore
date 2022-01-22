dfbf64d408971c68128bcd94b1d639f764b2067d
SupremeMarshal <vetaking@hotmail.com>
2022-01-22 14:37:28 -0500

    removed active lava

--- src/main/java/com/PlanetCore/init/ModFluids.java
+++ src/main/java/com/PlanetCore/init/ModFluids.java
@@ -28,7 +28,6 @@ public class ModFluids
 	public static final Fluid BRIGMANITE_LAVA_FLUID = new FluidLiquid("brigmanite_lava", new ResourceLocation("planetcore:blocks/brigmanite_lava_still"), new ResourceLocation("planetcore:blocks/brigmanite_lava_flow"));
 	public static final Fluid MAJORITE_LAVA_FLUID = new FluidLiquid("majorite_lava", new ResourceLocation("planetcore:blocks/majorite_lava_still"), new ResourceLocation("planetcore:blocks/majorite_lava_flow"));
 	public static final Fluid AMAZONITE_LAVA_FLUID = new FluidLiquid("amazonite_lava", new ResourceLocation("planetcore:blocks/amazonite_lava_still"), new ResourceLocation("planetcore:blocks/amazonite_lava_flow"));
-	public static final Fluid ACTIVE_LAVA_FLUID = new FluidLiquid("active_lava", new ResourceLocation("planetcore:blocks/active_lava_still"), new ResourceLocation("planetcore:blocks/active_lava_flow"));
 	public static final Fluid CORE_LAVA_FLUID = new FluidLiquid("core_lava", new ResourceLocation("planetcore:blocks/core_lava_still"), new ResourceLocation("planetcore:blocks/core_lava_flow"));
 	public static final Fluid SULFURIC_ACID_FLUID = new FluidLiquid("sulfuric_acid", new ResourceLocation("planetcore:blocks/sulfuric_acid_still"), new ResourceLocation("planetcore:blocks/sulfuric_acid_flow"));
 	public static final Fluid ONYX_LAVA_FLUID = new FluidLiquid("onyx_lava", new ResourceLocation("planetcore:blocks/onyx_lava_still"), new ResourceLocation("planetcore:blocks/onyx_lava_flow"));
@@ -55,7 +54,6 @@ public class ModFluids
 		registerFluid(BRIGMANITE_LAVA_FLUID);
 		registerFluid(MAJORITE_LAVA_FLUID);
 		registerFluid(AMAZONITE_LAVA_FLUID);
-		registerFluid(ACTIVE_LAVA_FLUID);
 		registerFluid(CORE_LAVA_FLUID);
 		registerFluid(SULFURIC_ACID_FLUID);
 		registerFluid(ONYX_LAVA_FLUID);
