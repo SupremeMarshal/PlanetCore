51fc94f3080f27a3a0d3d04f3ee0319ae605a5a9
Bartosz Skrzypczak <Barteks2x@gmail.com>
2023-08-05 21:23:00 +0200

    Fix water vaporization

--- src/main/java/com/PlanetCore/asm/mixin/MixinWorld.java
+++ src/main/java/com/PlanetCore/asm/mixin/MixinWorld.java
@@ -1,5 +1,8 @@
 package com.PlanetCore.asm.mixin;
 
+import com.PlanetCore.cwg.PlanetCoreWorldType;
+import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorld;
+import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorldServer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -7,8 +10,12 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.WorldType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,22 +25,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(World.class)
 public abstract class MixinWorld {
 
-    @Shadow public abstract boolean setBlockToAir(BlockPos pos);
+    @Shadow @Final public WorldProvider provider;
 
-    @Inject(at = @At("HEAD"), method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;I)Z", cancellable = true, remap = false)
+    @Shadow public abstract WorldType getWorldType();
+
+    @Inject(at = @At("HEAD"), method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/state/IBlockState;I)Z", cancellable =
+            true, remap = false)
     public void setBlockState(BlockPos pos, IBlockState state, int flags, CallbackInfoReturnable<Boolean> callback) {
-        if (state.getBlock() != Blocks.WATER || pos.getY() >= -512) return;
-        callback.setReturnValue(true);
+        if (!((ICubicWorld) this).isCubicWorld() ||
+                !(getWorldType() instanceof PlanetCoreWorldType) ||
+                provider.getDimensionType() != DimensionType.OVERWORLD) {
+            return;
+        }
+        if ((state.getBlock() != Blocks.WATER && state.getBlock() != Blocks.FLOWING_WATER) || pos.getY() >= -512) return;
+        callback.setReturnValue(false);
         if (!(((Object)this) instanceof WorldServer)) return;
         WorldServer world = (WorldServer)(Object)this;
         if (world.playerEntities.isEmpty()) return;
+        WorldServer serverWorld = (WorldServer) (Object) this;
         for (EntityPlayer player : world.playerEntities) {
             if (player.getDistanceSqToCenter(pos) > 16) continue;
             world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
-            for (int k = 0; k < 8; ++k) {
-                world.spawnParticle(EnumParticleTypes.SMOKE_LARGE,
-                        pos.getX() + Math.random(), pos.getY() + Math.random(), pos.getZ() + Math.random(), 0.0D, 0.0D, 0.0D);
-            }
+            serverWorld.spawnParticle(
+                    EnumParticleTypes.SMOKE_LARGE,
+                    pos.getX(), pos.getY(), pos.getZ(),
+                    8, 1, 1, 1, 0.0);
             return;
         }
     }
