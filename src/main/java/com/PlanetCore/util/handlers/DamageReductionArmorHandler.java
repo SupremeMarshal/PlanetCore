46b9403ed231cbc7ae7da1a1d547708e13d7a918
SupremeMarshal <vetaking@hotmail.com>
2023-08-18 18:20:35 -0400

    Lower gravity the deeper player is. (unfinished)

--- src/main/java/com/PlanetCore/util/handlers/DamageReductionArmorHandler.java
+++ src/main/java/com/PlanetCore/util/handlers/DamageReductionArmorHandler.java
@@ -1,6 +1,7 @@
 package com.PlanetCore.util.handlers;
 
 import com.PlanetCore.items.armor.ArmorBase;
+import com.PlanetCore.items.shields.Shield;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
@@ -24,11 +25,13 @@ public class DamageReductionArmorHandler {
             ItemStack chestplate = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
             ItemStack leggings = player.getItemStackFromSlot(EntityEquipmentSlot.LEGS);
             ItemStack boots = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
+            ItemStack shield = player.getItemStackFromSlot(EntityEquipmentSlot.OFFHAND);
 
             float helmetArmor = 0;
             float chestArmor = 0;
             float legsArmor = 0;
             float bootsArmor = 0;
+            float shieldArmor = 0;
 
             if (helmet.getItem() instanceof ArmorBase)
                 helmetArmor = ((ArmorBase) helmet.getItem()).extraArmor;
@@ -38,13 +41,19 @@ public class DamageReductionArmorHandler {
                 legsArmor = ((ArmorBase) leggings.getItem()).extraArmor;
             if (boots.getItem() instanceof ArmorBase)
                 bootsArmor = ((ArmorBase) boots.getItem()).extraArmor;
+            if (shield.getItem() instanceof Shield)
+                shieldArmor = ((Shield) shield.getItem()).extraArmor;
+
             // Calculate the total extra armor value
-            float totalExtraArmor = ( helmetArmor + chestArmor + legsArmor + bootsArmor );
+            float totalExtraArmor = ( helmetArmor + chestArmor + legsArmor + bootsArmor + shieldArmor );
 
             // Modify damage reduction calculation here
             //damage_done * 1 - (((armor)*0.06)/(1+0.06*(armor))
             float damage = event.getAmount();
             float modifiedDamage = event.getAmount() * (float) (1 - (((armor + toughness + totalExtraArmor) * 0.03) / (float) (1 + 0.03 * (armor + toughness + totalExtraArmor)))); // Example modification
+            if (modifiedDamage < 1) {
+                event.getEntityLiving().hurtTime = -1; // Disable hurt animation
+            }
             event.setAmount(modifiedDamage);
         }
     }
