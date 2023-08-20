b612e2d16d1ba60b2e83e5fb1fae7fffd2a8f19f
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-08-20 15:04:59 -0400

    fix item models

--- src/main/java/com/PlanetCore/util/handlers/RegistryHandler.java
+++ src/main/java/com/PlanetCore/util/handlers/RegistryHandler.java
@@ -74,14 +74,10 @@ public class RegistryHandler {
 
         ForgeRegistries.ITEMS.getValues().stream()
                 .filter(item -> MOD_ID.equals(item.getRegistryName().getNamespace())).forEach(item -> {
-            if (item.getHasSubtypes()) {
-                for (int i = 0; i < 3; i++) {
-                      //  ModelLoader.setCustomModelResourceLocation(item, i, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-                }
-            } else {
-                ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
-            }
-        });
+                    if (!item.getHasSubtypes()) {
+                        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+                    }
+                });
 
         RenderHandler.registerEntityRenders();
         RenderHandler.registerCustomMeshesAndStates();
