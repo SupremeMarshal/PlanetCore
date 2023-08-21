b01fc3820ab96f79e3e0abeb6c65c150c22a1412
SupremeMarshal <vetaking@hotmail.com>
2023-08-21 19:57:09 -0400

    Cleanup imports of player tick event

--- src/main/java/com/PlanetCore/util/handlers/PlayerTickEventHandler.java
+++ src/main/java/com/PlanetCore/util/handlers/PlayerTickEventHandler.java
@@ -1,21 +1,13 @@
 package com.PlanetCore.util.handlers;
 
-import com.PlanetCore.init.ModItems;
-import com.PlanetCore.items.Drills.IronDrill;
 import com.PlanetCore.util.Reference;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
 
 import static com.PlanetCore.util.handlers.TemperatureHandler.calcTemp;
 import static com.PlanetCore.util.handlers.TemperatureHandler.getDamage;
