f222bf0e66e06f99040624c95860aa220b0665b8
SupremeMarshal <vetaking@hotmail.com>
2023-09-16 10:31:57 -0400

    All supercompressed ores are indestructible. Only drills will be able to destroy them.

--- src/main/java/com/PlanetCore/blocks/BlocksBase.java
+++ src/main/java/com/PlanetCore/blocks/BlocksBase.java
@@ -1,27 +1,13 @@
 package com.PlanetCore.blocks;
 
 
-
-
 import com.PlanetCore.init.ModBlocks;
-import com.PlanetCore.util.IMetaName;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -48,7 +34,7 @@ public class BlocksBase extends BlockBase {
 
 	@Override
 	public float getBlockHardness(IBlockState blockState, World worldIn, BlockPos pos) {
-		return 1000000;
+		return -1;
 	}
 
 	@Override
