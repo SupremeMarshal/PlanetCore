c7c8a24191acf201b93194a21fa894e3e03418a3
SupremeMarshal <vetaking@hotmail.com>
2023-09-18 08:08:22 -0400

    Buff complete tools.

--- src/main/java/com/PlanetCore/init/ToolMaterials.java
+++ src/main/java/com/PlanetCore/init/ToolMaterials.java
@@ -10,17 +10,17 @@ public class ToolMaterials {
 
     public static final Map<Item.ToolMaterial, Float> relentlessMap = new HashMap<>();
 
-    public static final Item.ToolMaterial COMPLETE_IRON = createToolMaterial("complete_iron", 2, 500, 6.0F, 2.0F, 22, 1.5F);
+    public static final Item.ToolMaterial COMPLETE_IRON = createToolMaterial("complete_iron", 2, 500, 7.0F, 2.5F, 22, 1.5F);
     public static final Item.ToolMaterial SILVER = createToolMaterial("silver", 2, 33, 10.0F, 3.0F, 22, 1.5F);
-    public static final Item.ToolMaterial COMPLETE_SILVER = createToolMaterial("complete_silver", 2, 75, 10.0F, -0.5F, 22, 1.5F);
-    public static final Item.ToolMaterial COMPLETE_GOLD = createToolMaterial("complete_gold", 2, 75, 12.0F, 0.0F, 22, 1.5F);
-    public static final Item.ToolMaterial COMPLETE_DIAMOND = createToolMaterial("complete_diamond", 3, 3122, 8.0F, 3.0F, 22, 2.5F);
-    public static final Item.ToolMaterial TITANIUM = createToolMaterial("titanium", 4, 500, 12.0F, 4.0F, 22, 2.5F);
-    public static final Item.ToolMaterial COMPLETE_TITANIUM = createToolMaterial("complete_titanium", 4, 1000, 12.0F, 4.0F, 22, 2.5F);
+    public static final Item.ToolMaterial COMPLETE_SILVER = createToolMaterial("complete_silver", 2, 75, 11.0F, 0F, 22, 1.5F);
+    public static final Item.ToolMaterial COMPLETE_GOLD = createToolMaterial("complete_gold", 2, 75, 13.0F, 0.5F, 22, 1.5F);
+    public static final Item.ToolMaterial COMPLETE_DIAMOND = createToolMaterial("complete_diamond", 3, 3122, 9.0F, 3.5F, 22, 2.5F);
+    public static final Item.ToolMaterial TITANIUM = createToolMaterial("titanium", 4, 500, 12.0F, 4.5F, 22, 2.5F);
+    public static final Item.ToolMaterial COMPLETE_TITANIUM = createToolMaterial("complete_titanium", 4, 1000, 13.0F, 4.5F, 22, 2.5F);
     public static final Item.ToolMaterial URANIUM = createToolMaterial("uranium", 4, 120, 15.0F, 5.0F, 22, 3.0F);
-    public static final Item.ToolMaterial COMPLETE_URANIUM = createToolMaterial("complete_uranium", 4, 240, 15.0F, 5.0F, 22, 3.0F);
+    public static final Item.ToolMaterial COMPLETE_URANIUM = createToolMaterial("complete_uranium", 4, 240, 16.0F, 5.5F, 22, 3.0F);
     public static final Item.ToolMaterial TUNGSTEN = createToolMaterial("tungsten", 5, 2000, 20.0F, 6.0F, 22, 3.5F);
-    public static final Item.ToolMaterial COMPLETE_TUNGSTEN = createToolMaterial("complete_tungsten", 5, 4000, 20.0F, 6.0F, 22, 3.5F);
+    public static final Item.ToolMaterial COMPLETE_TUNGSTEN = createToolMaterial("complete_tungsten", 5, 4000, 21.0F, 6.5F, 22, 3.5F);
     public static final Item.ToolMaterial RUBY = createToolMaterial("ruby", 6, 3122, 30.0F, 7.0F, 22, 4F);
     public static final Item.ToolMaterial SAPPHIRE = createToolMaterial("sapphire", 7, 3122, 45.0F, 8.0F, 22, 4F);
     public static final Item.ToolMaterial MAJORITE = createToolMaterial("majorite", 8, 3122, 60.0F, 10.0F, 22, 4);
