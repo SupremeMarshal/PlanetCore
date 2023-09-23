aa139830468e9388f52a38a5a65b5943ac7095cd
SupremeMarshal <vetaking@hotmail.com>
2023-09-23 14:57:11 -0400

    Update build.gradle (not working)

--- src/main/java/com/PlanetCore/util/Reference.java
+++ src/main/java/com/PlanetCore/util/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "planetcore";
 	public static final String NAME = "Planetcore";
-	public static final String VERSION = "1.3.2-Beta";
+	public static final String VERSION = "1.3.3-Beta";
 	public static final String ACCEPTED_VERSION = "[1.12.2]";
 	public static final String CLIENT = "com.PlanetCore.proxy.ClientProxy";
 	public static final String SERVER = "com.PlanetCore.proxy.CommonProxy";
